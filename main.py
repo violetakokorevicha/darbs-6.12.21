d6749ffa2a2abdec315d4140ed3cf90e1ece55e3
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2021-12-06 08:20:59 +0000

    2 darbs

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-def lenkuparbaude(len1, len2, len3):
-  rezultats = False
-  if len1 + len2 + len3 == 180:
-    rezultats = True
-  return rezultats
-
-len1 = int(input('Ievadiet 1 lenki:'))
-len2 = int(input('Ievadiet 2 lenki:'))
-len3 = int(input('Ievadiet 3 lenki:'))
-rezultats = lenkuparbaude(len1, len2, len3)
-if rezultats:
- print('Trijsturis eksiste')
-else:
-  print('Trijsturis neeksiste')
+def noteikt_diapazonu(d1, d2, sk):
+  rezultats = 'Skaitlis nav diapazona!'
+  if d1>=sk>=d2:
+    rezultats = 'Skaitlis ir diapazona!'
+  return rezultats 
+  
+  
+  
+  
+d1  = int(input('Ievadi diapazona sakumu:'))
+d2 = int(input('Ievadi diapazona beigas:'))
+sk = int(input('Ievadi skaitli:'))
+rez = noteikt_diapazonu(d1, d2, sk)
+print(rez)
